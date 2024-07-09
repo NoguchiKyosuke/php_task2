fc8319d15519c7f3f1170a95be5ba67291006910
NoguchiKyosuke <nk20050709@yahoo.co.jp>
2024-07-09 16:41:29 +0900

    change form

--- index.php
+++ index.php
@@ -22,37 +22,56 @@
         echo "Connection failed: " . $e->getMessage();
     }
 
-    $sql = 'SELECT username, reponame FROM repos';
+    $sql = 'SELECT issue_id, title, label, priority, status, issue_commit, complete_commit FROM issues';
     $stmt = $conn->query($sql);
     $rows = $stmt->fetchAll();
 ?>
 
 
     <form action="index.php" method="POST">
+        <label for="Title">イシューID</label>
+        <input type="number" name="Issue_ID" id="Issue_ID" required><br>
+        
         <label for="Title">イシュータイトル</label>
-        <input type="text" name="Title" id="Title" repuired><br>
+        <input type="text" name="Title" id="Title" required><br>
+
 
         <label for="Label">選択してください</label><br>
-        <input type="radio" name="Label" value="bug" repuired>バグ<br>
-        <input type="radio" name="Label" value="repuirement" repuired>機能要求<br>
+        <input type="radio" name="Label" value="bug" required>バグ<br>
+        <input type="radio" name="Label" value="repuirement" required>機能要求<br>
 
         <label for="Rank">優先順位</label>
-        <input type="number" name="Rank" id="Rank" repuired><br>
+        <input type="number" name="Rank" id="Rank" required><br>
 
-        <label for="ID">イシューコミットID</label>
-        <input type="text" name="ID" id="ID" repuired><br>
-        
+        <label for="Status">進捗状況</label><br>
+        <input type="radio" name="Status" id="Not_Started" required>未着手<br>
+        <input type="radio" name="Status" id="Started" required>着手中<br>
+        <input type="radio" name="Status" id="Complete" required>完了<br>
+
+        <label for="Commit_ID">イシューコミットID</label>
+        <input type="text" name="Commit_ID" id="Commit_ID" required><br>
+
+        <label for="Complete_ID">完了コミット</label>
+        <input type="text" name="Complete_ID" id="Complete_ID" required><br>
+
         <input type="submit" value="submit">
     </form>
 
     <?php
+    
+
     if ($rows) {
         echo '<table>';
-        echo '<tr><th>username</th><th>reponame</th></tr>';
+        echo '<tr><th>イシューID</th><th>タイトル</th><th>ラベル</th><th>優先順位</th><th>状態</th><th>イシューコミットID</th><th>完了コミットID</th></tr>';
         foreach ($rows as $row) {
             echo '<tr>';
-            echo '<td>' . htmlspecialchars($row['username']) . '</td>';
-            echo '<td>' . htmlspecialchars($row['reponame']) . '</td>';
+            echo '<td>' . htmlspecialchars($row['issue_id']) . '</td>';
+            echo '<td>' . htmlspecialchars($row['title']) . '</td>';
+            echo '<td>' . htmlspecialchars($row['label']) . '</td>';
+            echo '<td>' . htmlspecialchars($row['priority']) . '</td>';
+            echo '<td>' . htmlspecialchars($row['status']) . '</td>';
+            echo '<td>' . htmlspecialchars($row['issue_commit']) . '</td>';
+            echo '<td>' . htmlspecialchars($row['complete_commit']) . '</td>';
             echo '</tr>';
         }
         echo '</table>';
