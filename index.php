7a6fc91df8a993ef37a2f1e1adbf8790a61f046e
NoguchiKyosuke <nk20050709@yahoo.co.jp>
2024-07-10 02:41:17 +0900

    insert form input into database

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <title>イシュー管理システム</title>
 
-<head>
+</head>
 
 <body>
 
@@ -15,16 +15,12 @@
     $dbname = "ISSUE_MANAGEMENT";
 
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
-
-    $sql = 'SELECT issue_id, title, label, priority, status, issue_commit, complete_commit FROM issues';
-    $stmt = $conn->query($sql);
-    $rows = $stmt->fetchAll();
 ?>
 
 
@@ -37,16 +33,16 @@
 
 
         <label for="Label">選択してください</label><br>
-        <input type="radio" name="Label" value="バグ" required>バグ<br>
-        <input type="radio" name="Label" value="機能要求" required>機能要求<br>
+        <input type="radio" name="Label" value="bug" required>バグ<br>
+        <input type="radio" name="Label" value="feature" required>機能要求<br>
 
         <label for="Rank">優先順位</label>
         <input type="number" name="Rank" id="Rank" required><br>
 
         <label for="Status">進捗状況</label><br>
-        <input type="radio" name="Status" id="Not_Started" required>未着手<br>
-        <input type="radio" name="Status" id="Started" required>着手中<br>
-        <input type="radio" name="Status" id="Complete" required>完了<br>
+        <input type="radio" name="Status" value="not_Started" required>未着手<br>
+        <input type="radio" name="Status" value="in_progress" required>着手中<br>
+        <input type="radio" name="Status" value="completed" required>完了<br>
 
         <label for="Commit_ID">イシューコミットID</label>
         <input type="text" name="Commit_ID" id="Commit_ID" required><br>
@@ -54,18 +50,37 @@
         <label for="Complete_ID">完了コミット</label>
         <input type="text" name="Complete_ID" id="Complete_ID" required><br>
 
-        <input type="submit" value="submit">
+        <input type="submit" id="submit" value="submit">
     </form>
 
     <?php
 
-    $issue_id = $_POST['Issue_ID'];    
-    $title = $_POST['Title'];    
-    $label = $_POST['Label'];    
-    $rank = $_POST['Rank'];    
-    $status = $_POST['Status'];    
-    $commit_id = $_POST['Commit_ID'];    
-    $complete_id = $_POST['Complete_ID'];    
+    if(isset($_POST['submit'])){
+        $issue_id = $_POST['Issue_ID'];
+        $title = $_POST['Title'];
+        $label = $_POST['Label'];
+        $priority = $_POST['Rank'];
+        $status = $_POST['Status'];
+        $commit_id = $_POST['Commit_ID'];
+        $complete_id = $_POST['Complete_ID'];
+
+        $sql = "INSERT INTO issues(issue_id, title, label, priority, status, issue_commit, complete_commit) VALUES(:issue_id, :title, :label, :priority, :status, :commit_id, :complete_id);";
+        $stmt = $pdo -> prepare($sql);
+        $stmt -> bindParam(':issue_id', $issue_id);
+        $stmt -> bindParam(':title', $title);
+        $stmt -> bindParam(':label', $label);
+        $stmt -> bindParam(':priority', $priority);
+        $stmt -> bindParam(':status', $status);
+        $stmt -> bindParam(':commit_id', $commit_id);
+        $stmt -> bindParam(':complete_id', $complete_id);
+
+        $stmt -> execute();
+    }
+
+    $sql = 'SELECT issue_id, title, label, priority, status, issue_commit, complete_commit FROM issues;';
+    $stmt = $pdo->query($sql);
+    $rows = $stmt->fetchAll();
+   
 
     if ($rows) {
         echo '<table>';
